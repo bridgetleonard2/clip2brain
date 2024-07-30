74e610d89051522a4f4bae1ce3041362da1b54ad
Bridget Leonard <bll313@g3050.hyak.local>
2024-07-30 09:13:17 -0700

    add image array

--- get_stimuli.py
+++ get_stimuli.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 
 subj = 1
 project_output_dir = 'output'
@@ -10,3 +11,20 @@ all_coco_ids = np.load("%s/coco_ID_of_repeats_subj%02d.npy" %
 
 all_images_paths = ["%s/%s.jpg" % (stimuli_dir, id) for id in all_coco_ids]
 print("Number of Images: {}".format(len(all_images_paths)))
+
+# check brain data shape
+br_data = np.load('output/cortical_voxels/averaged_cortical_responses_zscored_by_run_subj%02d.npy' % subj)
+print("Brain data shape:", br_data.shape)
+
+# generate an image array
+image_array = np.array([np.array(Image.open(img_path).convert('RGB'))
+                        for img_path in all_images_paths])
+print("Image array shape: {}".format(image_array.shape))
+
+trial_mask = np.sum(np.isnan(br_data), axis=1) <= 0
+br_data = br_data[trial_mask, :]
+
+image_array = image_array[trial_mask, :, :, :]
+
+print("remove dead trials; brain data shape:", br_data.shape)
+print("image array shape:", image_array.shape)
